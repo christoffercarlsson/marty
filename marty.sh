0ae54d2863247d7cfc4de1fef11bbe308af5c26a
Christoffer Carlsson <cc@christofferc.com>
2022-11-22 15:44:25 +0100

    Keep rsync backups and archives in the same directory

--- marty.sh
+++ marty.sh
@@ -3,19 +3,19 @@
 set -e
 
 SOURCE=""
-SYNC=""
 DEST=""
 
 DAYS=30
 
 BACKUP_PREFIX=$(basename $SOURCE)
+BACKUP_PATH="$DEST/$BACKUP_PREFIX"
 
 get_archive_path() {
-  echo "$DEST/$BACKUP_PREFIX-$1.tar.gz"
+  echo "$BACKUP_PATH-$1.tar.gz"
 }
 
 get_sync_path() {
-  echo "$SYNC/$BACKUP_PREFIX/$1"
+  echo "$BACKUP_PATH/$1"
 }
 
 remove_backup() {
@@ -26,7 +26,7 @@ remove_backup() {
 
 remove_old_backups() {
   local removal_cutoff_date=$(date -d "$DAYS days ago" +%Y%m%d)
-  local backup_dates=$(find -E $SYNC/$BACKUP_PREFIX -type d -regex ".*/[0-9]{8}" -exec bash -c 'basename "{}"' \;)
+  local backup_dates=$(find -E $BACKUP_PATH -type d -regex ".*/[0-9]{8}" -exec bash -c 'basename "{}"' \;)
   for backup_date in $backup_dates
   do
     if [ $removal_cutoff_date -gt $backup_date ]
@@ -40,7 +40,6 @@ remove_old_backups() {
 perform_archive() {
   local archive_path=$(get_archive_path $1)
   local sync_path=$(get_sync_path $1)
-  mkdir -p $DEST
   tar -C $sync_path --hard-dereference -czf $archive_path .
 }
 
